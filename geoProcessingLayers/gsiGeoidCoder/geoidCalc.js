f30e452bc15bafb1b1d61917dbff4ecf8be3a521
Satoru Takagi <satakagi@gmail.com>
2025-06-09 11:20:05 +0900

    Update vectorGisLayer

--- geoProcessingLayers/gsiGeoidCoder/geoidCalc.js
+++ geoProcessingLayers/gsiGeoidCoder/geoidCalc.js
@@ -7,7 +7,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-//
+// 
 // History:
 // 2022/02/08 : 1st rev.
 
